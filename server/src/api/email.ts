042ec34d138676a5ddd1e6689544cde89310484d
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-19 01:34:19 -0700

    prepare for user auth: integrate sendgrid, install bcrypt

---
+++ server/src/api/email.ts
@@ -0,0 +1,11 @@
+import sendgrid from "@sendgrid/mail";
+import {reqEnv} from "./env";
+
+
+sendgrid.setApiKey(reqEnv("SENDGRID_API_KEY"))
+
+export async function sendMail(to: string, from: string, subject: string, html: string) {
+    return sendgrid.send({
+        to, from, subject, html});
+}
+
