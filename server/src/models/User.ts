f6fa2e07c61b8df63bace7c5dc268cca73c867a0
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-19 01:34:50 -0700

    add basic fields in User schema

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,8 +1,27 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
 
+const userSchema = new Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: {
+        type: String,
+        required: true
+    },
+    isValidated: { type: Boolean, required: true, default: false },
+
+    userType: { enum: ["user", "staff", "admin"]}
+}, {
+    timestamps: true,
+    toJSON: {
+        // Remove sensitive & unnecessary data when sending to client-side
+        transform: function(doc, ret) {
+            delete ret.password;
+            delete ret.isValidated;
+            return ret;
+        }
+    }
 });
 
 export default mongoose.model("User", userSchema);
