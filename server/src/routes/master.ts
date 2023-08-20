16afc0a9764b1d2b9fcd04ccc94bdfc9a2c8737b
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-19 23:56:08 -0700

    add custom server error handler

--- server/src/routes/master.ts
+++ server/src/routes/master.ts
@@ -2,11 +2,42 @@
  * Master router that mounts all other routes
  */
 
-import express from "express";
+import express, {Request, Response, NextFunction} from "express";
+import {ServerError} from "../utilities/errors";
+
 const router = express.Router();
+
+import authRouter from "./api/auth";
+
+router.use("/api/auth", authRouter);
+
+
+// error handler
+router.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+    if (res.headersSent) {
+        return next(err)
+    }
+
+    if (err instanceof ServerError) {
+        res.status(err.statusCode);
+        res.statusMessage = err.message;
+
+        console.error(err.stack);
+    } else if (err instanceof Error) {
+        res.status(500);
+        console.error(err.stack);
+    } else {
+        res.status(500);
+        console.error(err);
+    }
+
+    res.json({ error: err });
+});
+
 
 router.all("*", (req, res, next) => {
     res.sendFile("/index.html");
 });
+
 
 export default router;
