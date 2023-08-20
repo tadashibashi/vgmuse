27c640046d210fca650b627f34f8ac4f463f1fcf
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-20 02:04:26 -0700

    Style Alert component

--- frontend/src/components/Alert.tsx
+++ frontend/src/components/Alert.tsx
@@ -56,19 +56,23 @@ export default function Alert({title, setIsVisible, children, type, className}: 
     // bg-green-50 bg-yellow-50 bg-blue-50 bg-red-50
     // hover:bg-green-100 hover:bg-yellow-100 hover:bg-blue-100 hover:bg-red-100
     // text-green-400 text-yellow-400 text-blue-400 text-red-400
-    // text-green-600 text-yellow-600 text-blue-600 text-red-600
+    // text-green-700 text-yellow-700 text-blue-700 text-red-700
     // text-green-800 text-yellow-800 text-blue-800 text-red-800
     // focus:ring-offset-green-50 focus:ring-offset-yellow-50 focus:ring-offset-blue-50 focus:ring-offset-red-50
     // focus:ring-green-600 focus:ring-yellow-600 focus:ring-blue-600 focus:ring-red-600
     return (
-        <div className={`rounded-md bg-${color}-50 p-4 text-${color}-600 ${className ? className : ""}`}>
+        <div className={`rounded-md bg-${color}-50 p-4 text-${color}-700 ${className ? className : ""}`}>
             <div className="flex">
                 <div className="flex-shrink-0">
                     <Icon className={`h-5 w-5 text-${color}-400`} aria-hidden="true" />
                 </div>
                 <div className="ml-3">
-                    {title && <h3 className={`text-sm font-medium text-${color}-800`}>{title}</h3>}
-                    {children}
+                    <div className="mb-1">
+                        {title && <h3 className={`text-sm font-medium text-${color}-800`}>{title}</h3>}
+                    </div>
+                    <div>
+                        {children}
+                    </div>
                 </div>
 
                 {
