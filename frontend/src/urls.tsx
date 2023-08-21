63533457eb85e798a718b6bcc44046baefff3f35
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-21 02:52:13 -0700

    add landing page to urls

--- frontend/src/urls.tsx
+++ frontend/src/urls.tsx
@@ -1,6 +1,7 @@
 import LogInPage from "./pages/auth/LogInPage";
 import SignUpPage from "./pages/auth/SignUpPage";
 import React from "react";
+import LandingPage from "./pages/LandingPage.tsx";
 
 export interface URLDirectory {
     [key: string]: URLInfo
@@ -20,6 +21,11 @@ const urls = {
     userSignUp: {
         path: "/auth/signup",
         page: <SignUpPage />
+    } as URLInfo,
+
+    landingPage: {
+        path: "/",
+        page: <LandingPage />
     } as URLInfo,
 };
 
