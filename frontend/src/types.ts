97814d0e34ddd1fe296423ab6c19b5d73a702ada
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-22 11:49:31 -0700

    add frontend unknown type validation file

---
+++ frontend/src/types.ts
@@ -0,0 +1,49 @@
+
+export interface IError {
+    name: string;
+    message: string;
+}
+
+export interface IServerError extends IError {
+    statusCode: number;
+}
+
+
+/**
+ * Namespace for duck-type validation of unknown data,
+ * mainly for JSON data received from the backend.
+ */
+export namespace Is {
+    export function error(val: any): val is IError {
+        return val && typeof val === "object" &&
+            typeof val.name === "string" &&
+            typeof val.message === "string";
+    }
+
+    export function serverError(val: any): val is IServerError {
+        return error(val) && typeof (val as any).statusCode === "number";
+    }
+
+
+    export function errorContainer(val: any): val is {error: IError} {
+        return val && typeof val === "object" && error(val.error);
+    }
+
+    export function serverErrorContainer(val: any): val is {error: IServerError} {
+        return val && typeof val === "object" && serverError(val.error);
+    }
+
+    export function errorsContainer(val: any): val is {errors: Record<string, IError>} {
+        if (!(val && typeof val === "object" &&
+            typeof val.errors === "object"))
+            return false;
+
+        for (const err of Object.values(val)) {
+            if (!error(err))
+                return false;
+        }
+
+        return true;
+    }
+}
+
