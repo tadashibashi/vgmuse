853aecb24712ec389e9beda7ca6e64f7828d246c
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-22 01:39:32 -0700

    use new logo

--- frontend/src/components/AuthHeader.tsx
+++ frontend/src/components/AuthHeader.tsx
@@ -1,14 +1,17 @@
-export default function AuthHeader({headerText}: {headerText: string}) {
+export default function AuthHeader({headerText, className}: {headerText: string, className?: string}) {
     return (
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-                className="mx-auto h-20 w-auto filter"
-                src="/images/logo-v1.svg"
-                alt="VGMuse Logo"
-            />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                {headerText}
-            </h2>
+        <div className={className || ""}>
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <img
+                    className="mx-auto h-20 w-auto filter pointer-events-none"
+                    src="/images/logo-v2.png"
+                    alt="VGMuse Logo"
+                />
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 pointer-events-none">
+                    {headerText}
+                </h2>
+            </div>
         </div>
+
     );
 }
