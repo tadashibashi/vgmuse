11dfede0dc02392dcb904a4893f4a0e97413a32e
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-22 01:36:52 -0700

    update sign up and login page to animate error messages

--- frontend/src/pages/auth/LogInPage.tsx
+++ frontend/src/pages/auth/LogInPage.tsx
@@ -2,70 +2,113 @@ import AuthHeader from "../../components/AuthHeader";
 import Form from "../../components/Form.tsx";
 import urls from "../../urls";
 
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import {FormErrors} from "../../utility/formValidation";
-import Alert from "../../components/Alert.tsx";
-import {useState} from "react";
+import Alert from "../../components/Alert";
+import React, {useEffect, useState} from "react";
+import {getBackendUser} from "../../api/auth";
+import Spinner from "../../components/icons/Spinner";
 
+import {Transition} from "@headlessui/react";
+import FadeIn from "../../components/FadeInOut";
+import {EnvelopeIcon, LockClosedIcon} from "@heroicons/react/24/outline";
 
 export default function LogInPage() {
     const [errors, setErrors] = useState<string[]>([]);
+    const [showErrors, setShowErrors] = useState<boolean>(false);
+    const [showPage, setShowPage] = useState<boolean>(false);
+    const [sending, setSending] = useState<boolean>(false);
+    const navigate = useNavigate();
+
     function onValidationError(errors: FormErrors) {
         setErrors(Object.values(errors).map(err => err.message));
+        setShowErrors(true);
+        setSending(false);
     }
 
     function onSuccess(data: unknown) {
+        setSending(false);
+        setShowErrors(false);
         console.log(data);
+        navigate(urls.landingPage.path);
+    }
+
+    function shouldSubmit() {
+        setSending(true);
+        return true;
     }
 
     return (
+        <FadeIn>
+
+
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
             <AuthHeader headerText="Log in to your account" />
 
 
-
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm border-gray-100">
-                {errors.length !== 0 &&
+                <Transition show={showErrors}
+                    enter="transition-all ease-in-out duration-150"
+                    enterFrom="opacity-0 scale-0 max-h-0"
+                    enterTo="opacity-100 scale-100 max-h-full"
+                    leave="transition-all ease-in-out duration-500"
+                    leaveFrom="opacity-100 max-h-full scale-100"
+                    leaveTo="opacity-0 max-h-0 scale-0"
+                >
                     <Alert type="error"
                            title={errors.length > 1 ? `There were ${errors.length} errors with your submission` : `There was an error with your submission`}
                            className="mb-4"
-                           setIsVisible={(value) => setErrors([])}>
+                           setIsVisible={(value) => {setShowErrors(value);}}>
                         <ul role="list" className="text-sm list-disc space-y-1 pl-5">
                             {errors.map((error, i) => <li key={"error-" + i}>{error}</li>)}
                         </ul>
                     </Alert>
-                }
+                </Transition>
 
-                <Form action="/api/auth/login" onValidationError={onValidationError} onSuccess={onSuccess} className="space-y-6" method="POST">
+                <Form action="/api/auth/login" onValidationError={onValidationError} onSuccess={onSuccess} shouldSubmit={shouldSubmit} className="space-y-6" method="POST">
                     <div className="relative -space-y-px rounded-md shadow-sm">
                         <div className="pointer-events-none absolute inset-0 z-10 rounded-md ring-1 ring-inset ring-gray-300" />
                         <div>
                             <label htmlFor="email-address" className="sr-only">
                                 Email address
                             </label>
-                            <input
-                                id="email-address"
-                                name="email"
-                                type="email"
-                                autoComplete="email"
-                                required
-                                className="relative block w-full rounded-t-md border-0 py-1.5 px-2 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
-                                placeholder="Email address"
-                            />
+                            <div className="relative">
+                                <div className="pointer-events-none absolute z-50 inset-y-0 -left-2 flex items-center pl-3">
+                                    <EnvelopeIcon className=" h-8 p-1.5 text-gray-200" aria-hidden="true" />
+                                </div>
+
+                                <input
+                                    id="email-address"
+                                    name="email"
+                                    type="email"
+                                    autoComplete="email"
+                                    required
+                                    className="relative w-full rounded-t-md border-0 py-1.5 px-10 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
+                                    placeholder="Email address"
+                                />
+                            </div>
+
                         </div>
                         <div>
                             <label htmlFor="password" className="sr-only">
                                 Password
                             </label>
-                            <input
-                                id="password"
-                                name="password"
-                                type="password"
-                                autoComplete="current-password"
-                                required
-                                className="relative block w-full rounded-b-md border-0 py-1.5 px-2 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
-                                placeholder="Password"
-                            />
+                            <div className="relative">
+                                <div className="pointer-events-none absolute z-50 inset-y-0 -left-2 flex items-center pl-3">
+                                    <LockClosedIcon className="h-8 text-gray-200 p-1.5" aria-hidden="true" />
+                                </div>
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    className="relative block w-full rounded-b-md border-0 py-1.5 px-10 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
+                                    placeholder="Password"
+                                />
+                            </div>
+
+
                         </div>
                     </div>
 
@@ -94,18 +137,19 @@ export default function LogInPage() {
                             type="submit"
                             className="flex w-full justify-center rounded-md bg-violet-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600"
                         >
-                            Sign in
+                        {sending && <Spinner className="me-2" bgClass="fill-violet-900" fgClass="fill-violet-200"/>} {sending ? "Processing..." : "Log in"}
                         </button>
                     </div>
                 </Form>
 
                 <p className="mt-10 text-center text-sm text-gray-500">
-                    Not a member?{' '}
+                    <span className="pointer-events-none">Not a member?{' '}</span>
                     <Link to={urls.userSignUp.path} className="font-semibold leading-6 text-violet-600 hover:text-violet-500">
                         Sign up here
                     </Link>
                 </p>
             </div>
         </div>
+        </FadeIn>
     );
 }
--- frontend/src/pages/auth/SignUpPage.tsx
+++ frontend/src/pages/auth/SignUpPage.tsx
@@ -4,30 +4,49 @@ import urls from "../../urls";
 
 import {Link, useNavigate} from "react-router-dom";
 import Alert from "../../components/Alert.tsx";
-import {useState} from "react";
+import React, {useRef, useState} from "react";
 import {FormErrors, hasFormErrors} from "../../utility/formValidation.ts";
 
-
+import {ArrowPathIcon} from "@heroicons/react/24/solid";
+import Spinner from "../../components/icons/Spinner.tsx";
 
 
 export default function SignUpPage() {
     const [errors, setErrors] = useState<string[]>([]);
+
+    const [sending, setSending] = useState<boolean>(false);
+
+    // flag to prevent multiple submits
+    const canSubmitRef = useRef<boolean>(true);
+
     const navigate = useNavigate();
+
 
     function onSuccess(data: unknown) {
         console.log("received:", data);
+        setSending(false);
         // TODO: set user here
-        navigate("/");
+        navigate(urls.validationEmailSent.path);
     }
 
     function onValidationError(errors: FormErrors) {
         setErrors(Object.values(errors).map(value => value.message));
+        canSubmitRef.current = true;
+        setSending(false);
     }
 
     function shouldSubmit(formData: FormData): boolean {
+        if (!canSubmitRef.current) return false;
+        canSubmitRef.current = false;
+
         const matching = formData.get("password") === formData.get("password-confirm");
         if (!matching)
             setErrors(["passwords are mismatched"]);
+        else {
+            setSending(true);
+
+        }
+
         return matching;
     }
 
@@ -58,7 +77,7 @@ export default function SignUpPage() {
                                     id="username"
                                     name="username"
                                     type="text"
-                                    autoComplete="none"
+                                    autoComplete="off"
                                     required
                                     className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
                                 />
@@ -123,13 +142,13 @@ export default function SignUpPage() {
                             type="submit"
                             className="flex w-full justify-center rounded-md bg-violet-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600"
                         >
-                            Sign up
+                            {sending && <Spinner className="me-2" bgClass="fill-violet-900" fgClass="fill-violet-200"/>} {sending ? "Processing..." : "Sign up"}
                         </button>
                     </div>
                 </Form>
 
                 <p className="mt-10 text-center text-sm text-gray-500">
-                    Already a member?{' '}
+                    <span className="pointer-events-none">Already a member?{' '}</span>
                     <Link to={urls.userLogin.path} className="font-semibold leading-6 text-violet-600 hover:text-violet-500">
                         Log in here
                     </Link>
