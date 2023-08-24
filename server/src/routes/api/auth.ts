a6a3ac79592a0d6532be37c1e905648358ff2332
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-24 09:44:48 -0700

    loginRequired on resend validation route

--- server/src/routes/api/auth.ts
+++ server/src/routes/api/auth.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import * as authCtrl from "../../controllers/api/auth";
 import * as formParser from "../../middleware/formParser";
+import {loginRequired} from "../../middleware/loginRequired";
 
 // Base URL: "/api/auth"
 const router = express.Router();
@@ -11,5 +12,7 @@ router.post("/signup", formParser.textOnly(),  authCtrl.signup);
 
 router.get("/activate/:token", authCtrl.activateAccount);
 router.get("/user", authCtrl.refreshUser);
+
+router.post("/resend-verification", loginRequired(), authCtrl.resendVerificationEmail);
 
 export default router;
