b5c3413ab3e2ccbd469897312d8c810b50de388b
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-25 16:55:33 -0700

    stub vgm controller functions

---
+++ server/src/controllers/api/vgm.ts
@@ -0,0 +1,26 @@
+/**
+ * Create a VGM file
+ */
+export const createOne = function(req, res, next) {
+
+
+
+} as VGMuse.MiddlewareFunction;
+
+export const search = function(req, res, next) {
+    const q = req.params["q"];
+
+    // default, no q param gets latest tracks
+
+
+} as VGMuse.MiddlewareFunction;
+
+export const updateOne = function(req, res, next) {
+
+
+} as VGMuse.MiddlewareFunction;
+
+export const deleteOne = function(req, res, next) {
+
+
+} as VGMuse.MiddlewareFunction;
