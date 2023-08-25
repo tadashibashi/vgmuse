2feef0847d73998ea357482c14b2afaca8902f6b
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-25 16:55:13 -0700

    change readFile to convert to byte array, add comments

--- server/src/api/s3.ts
+++ server/src/api/s3.ts
@@ -1,44 +1,33 @@
-import {
-    S3Client,
-    PutObjectCommand, GetObjectCommand, DeleteObjectCommand,
-
-} from "@aws-sdk/client-s3";
+import {DeleteObjectCommand, GetObjectCommand, PutObjectCommand, S3Client,} from "@aws-sdk/client-s3";
 import {reqEnv} from "../lib/env";
 import {StreamingBlobPayloadInputTypes} from "@smithy/types";
 
-
-const BUCKET = reqEnv("S3_BUCKET");
+// ========== types ==========
 
 export type UploadFile = StreamingBlobPayloadInputTypes;
 
-let _s3: S3Client | null = null;
-function getS3Client() {
-    if (!_s3)
-        _s3 = new S3Client({
-            region: "auto",
-            endpoint: reqEnv("S3_ENDPOINT"),
-            credentials: {
-                accessKeyId: reqEnv("S3_ACCESS_KEY"),
-                secretAccessKey: reqEnv("S3_SECRET_ACCESS_KEY"),
-            },
-        });
-    return _s3;
-}
+// ========== constants ==========
+
+const BUCKET = reqEnv("S3_BUCKET");
+
+
+
+// ========== api ==========
 
 export async function uploadFile(key: string, body: UploadFile) {
     const s3 = getS3Client();
 
-    const res = await s3.send(new PutObjectCommand({
+    return await s3.send(new PutObjectCommand({
         Bucket: BUCKET,
         Key: key,
         Body: body,
     }));
-
-    console.log(res);
-
-    return res;
 }
 
+/**
+ * Delete a file from s3
+ * @param key
+ */
 export async function deleteFile(key: string) {
     const s3 = getS3Client();
 
@@ -52,6 +41,12 @@ export async function deleteFile(key: string) {
     return res;
 }
 
+/**
+ * Returns a byte array from file uploaded to s3, or
+ * null if it could not be found, or there was an error
+ *
+ * @param key
+ */
 export async function readFile(key: string) {
     const s3 = getS3Client();
 
@@ -60,5 +55,22 @@ export async function readFile(key: string) {
         Key: key,
     }));
 
-    return res.Body || null;
+    return res.Body ? res.Body.transformToByteArray() : null;
+}
+
+
+// ========== helpers ==========
+
+let _s3: S3Client | null = null;
+function getS3Client() {
+    if (!_s3)
+        _s3 = new S3Client({
+            region: "auto",
+            endpoint: reqEnv("S3_ENDPOINT"),
+            credentials: {
+                accessKeyId: reqEnv("S3_ACCESS_KEY"),
+                secretAccessKey: reqEnv("S3_SECRET_ACCESS_KEY"),
+            },
+        });
+    return _s3;
 }
