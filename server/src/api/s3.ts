72ffeda5ea65700ff87a290c5afc5c98ed35fafa
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-25 16:05:20 -0700

    create s3 api interface

---
+++ server/src/api/s3.ts
@@ -0,0 +1,62 @@
+import {
+    S3Client,
+    PutObjectCommand, GetObjectCommand, DeleteObjectCommand,
+
+} from "@aws-sdk/client-s3";
+import {reqEnv} from "../lib/env";
+import {StreamingBlobPayloadInputTypes} from "@smithy/types";
+
+
+const BUCKET = reqEnv("S3_BUCKET");
+
+let _s3: S3Client | null = null;
+function getS3Client() {
+    if (!_s3)
+        _s3 = new S3Client({
+            region: "auto",
+            endpoint: reqEnv("S3_ENDPOINT"),
+            credentials: {
+                accessKeyId: reqEnv("S3_ACCESS_KEY"),
+                secretAccessKey: reqEnv("S3_SECRET_ACCESS_KEY"),
+            },
+        });
+    return _s3;
+}
+
+export async function uploadFile(key: string, body: StreamingBlobPayloadInputTypes) {
+    const s3 = getS3Client();
+
+    const res = await s3.send(new PutObjectCommand({
+        Bucket: BUCKET,
+        Key: key,
+        Body: body,
+    }));
+
+    console.log(res);
+
+    return res;
+}
+
+export async function deleteFile(key: string) {
+    const s3 = getS3Client();
+
+    const res = await s3.send(new DeleteObjectCommand({
+        Bucket: BUCKET,
+        Key: key,
+    }));
+
+    console.log(res);
+
+    return res;
+}
+
+export async function readFile(key: string) {
+    const s3 = getS3Client();
+
+    const res = await s3.send(new GetObjectCommand({
+        Bucket: BUCKET,
+        Key: key,
+    }));
+
+    return res.Body || null;
+}
