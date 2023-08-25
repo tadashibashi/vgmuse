a0aa382235000350eb2e6d2a09eb56957119e658
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-25 06:44:44 -0700

    create custom useQueryStore hook

--- frontend/src/hooks/useQuery.ts
+++ frontend/src/hooks/useQuery.ts
@@ -1,8 +1,29 @@
 import {useLocation} from "react-router-dom";
-import {useMemo} from "react";
+import {useEffect, useMemo, useState} from "react";
 
+/**
+ * Caches query params
+ */
 export function useQuery() {
     const { search } = useLocation();
 
     return useMemo(() => new URLSearchParams(search), [search]);
 }
+
+
+/**
+ * Caches query params on component load
+ * @param stripQuery - whether to remove query string from address bar
+ */
+export const useQueryStore = (stripQuery: boolean = true) => {
+    const { pathname, search } = useLocation();
+    const query = useState(() => new URLSearchParams(search));
+
+    if (stripQuery) {
+        useEffect(() => {
+            window.history.replaceState(null, "", pathname);
+        }, []);
+    }
+
+    return query[0];
+};
