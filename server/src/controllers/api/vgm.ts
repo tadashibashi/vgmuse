6145a5027e12313294cc24dbfb805cb27382fd7c
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-26 22:38:45 -0700

    creates stub for rest of vgm controller functions

--- server/src/controllers/api/vgm.ts
+++ server/src/controllers/api/vgm.ts
@@ -1,12 +1,18 @@
 /**
  * Create a VGM file
+ * POST /api/vgm/
  */
 export const createOne = function(req, res, next) {
-
-
+    console.log(req.files);
+    return res.json(req.files);
 
 } as VGMuse.MiddlewareFunction;
 
+/**
+ * Search for VGM files
+ * GET /api/vgm/search
+ * Query parameters: q, search string
+ */
 export const search = function(req, res, next) {
     const q = req.params["q"];
 
@@ -15,12 +21,43 @@ export const search = function(req, res, next) {
 
 } as VGMuse.MiddlewareFunction;
 
+/**
+ * Update one VGM file
+ * PATCH /api/vgm/:username/:slug
+ */
 export const updateOne = function(req, res, next) {
+
 
 
 } as VGMuse.MiddlewareFunction;
 
+/**
+ * Delete one VGM file
+ * DELETE /api/vgm/:username/:slug
+ */
 export const deleteOne = function(req, res, next) {
+
+
+
+} as VGMuse.MiddlewareFunction;
+
+/**
+ * Read one VGM file
+ * GET /api/vgm/:username/:slug
+ */
+export const readOne = function(req, res, next) {
+
+
+
+} as VGMuse.MiddlewareFunction;
+
+
+/**
+ * Read a user's VGM files
+ * GET /api/vgm/:username
+ */
+export const readAll = function(req, res, next) {
+
 
 
 } as VGMuse.MiddlewareFunction;
