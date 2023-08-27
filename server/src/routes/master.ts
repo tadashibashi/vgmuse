5db14343294fbbd3f6381ab6ca7ee57ddbd5cbfc
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-27 03:27:44 -0700

    mount vgm routes in master router

--- server/src/routes/master.ts
+++ server/src/routes/master.ts
@@ -5,11 +5,14 @@
 import express from "express";
 import {ServerError} from "../lib/errors";
 import path from "path";
+
 const router = express.Router();
 
+import vgmRouter from "./api/vgm";
 import authRouter from "./api/auth";
 
 router.use("/api/auth", authRouter);
+router.use("/api/vgm", vgmRouter);
 
 router.all("*", (req, res, next) => {
     res.sendFile(path.join(process.cwd() + "/../frontend/dist/index.html"));
