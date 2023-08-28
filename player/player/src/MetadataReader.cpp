720787e1afc84a3115ac606986d5632eb36adc4d
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-28 14:54:01 -0700

    add metadata reader class

---
+++ player/player/src/MetadataReader.cpp
@@ -0,0 +1,124 @@
+#include "MetadataReader.h"
+#include "gme/Music_Emu.h"
+#include <stdexcept>
+
+struct vgmuse::MetadataReader::Impl {
+    Impl() : emu(), track_queried(-1), track_info() { }
+
+    ~Impl()
+    {
+        close();
+    }
+
+    error_t open_mem(const void *data, long size)
+    {
+        Music_Emu *temp_emu;
+        ERR_CHECK( gme_open_data(data, size, &temp_emu, 0) );
+
+        close();
+        emu = temp_emu;
+
+        // load first track out of convenience
+        ERR_CHECK(load_track_info(0));
+
+        return SUCCESS;
+    }
+
+    error_t load_track_info(int track) {
+        if (!emu) throw std::runtime_error("Emu is not loaded");
+        if (track == track_queried)
+            return SUCCESS;
+
+        gme_info_t *temp;
+
+        ERR_CHECK(gme_track_info(emu, &temp, track));
+
+        close_info();
+        track_info = temp;
+        track_queried = track;
+        return SUCCESS;
+    }
+
+    const gme_info_t *info() const {
+        return track_info;
+    }
+
+    [[nodiscard]]
+    int track_count() const {
+        return emu ? emu->track_count() : 0;
+    }
+
+    void close()
+    {
+        close_info();
+        if (emu)
+        {
+            gme_delete(emu);
+            emu = nullptr;
+            track_queried = -1;
+        }
+    }
+
+    void close_info() {
+        if (track_info)
+        {
+            delete track_info;
+            track_info = nullptr;
+        }
+
+    }
+
+    Music_Emu *emu;
+    mutable int track_queried;
+    mutable gme_info_t *track_info;
+};
+
+
+vgmuse::MetadataReader::MetadataReader() : m(new Impl)
+{
+
+}
+
+vgmuse::MetadataReader::~MetadataReader()
+{
+    delete m;
+}
+
+vgmuse::error_t vgmuse::MetadataReader::load_mem(const void *data, size_t byte_size)
+{
+    Music_Emu *emu;
+    ERR_CHECK(gme_open_data(data, byte_size, &emu, 44100));
+
+    if (m->emu)
+        gme_delete(m->emu);
+    m->emu = emu;
+
+    return SUCCESS;
+}
+
+const gme_info_t *vgmuse::MetadataReader::load_track_info(int track) const
+{
+    error_t result;
+    result = m->load_track_info(track);
+    if (result != SUCCESS)
+        return nullptr;
+
+    return m->info();
+}
+
+int vgmuse::MetadataReader::track_count() const
+{
+    return m->track_count();
+}
+
+vgmuse::error_t vgmuse::MetadataReader::load_file(const char *path)
+{
+    Music_Emu *temp;
+    ERR_CHECK(gme_open_file(path, &temp, 44100));
+
+    if (m->emu)
+        gme_delete(m->emu);
+    m->emu = temp;
+
+    return SUCCESS;
+}
---
+++ player/player/src/MetadataReader.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cstdio>
+#include "Error.h"
+#include "gme/gme.h"
+
+namespace vgmuse {
+    class MetadataReader {
+        struct Impl;
+    public:
+        MetadataReader();
+        ~MetadataReader();
+
+        error_t load_mem(const void *data, size_t byte_size);
+        error_t load_file(const char *path);
+
+        [[nodiscard]]
+        const gme_info_t *load_track_info(int track) const;
+
+        [[nodiscard]]
+        int track_count() const;
+    private:
+        Impl *m;
+    };
+}
+
