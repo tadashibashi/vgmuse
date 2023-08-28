9876d77e41444cb8f421fb88d4ae2e53c91c6441
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-28 14:58:33 -0700

    wrap C functions in JS client-side

--- frontend/src/services/vgm.ts
+++ frontend/src/services/vgm.ts
@@ -1,18 +1,69 @@
-import {FileCache} from "../api/webassembly";
+import {FileCache, free, store} from "../api/webassembly";
 
 const _cache = new FileCache();
 
-const _loadData = Module.cwrap("load_data", "string", ["number", "number"]);
+// Wrap C functions
+
+const _load_vgm = Module.cwrap("load_vgm", "string", ["string"]);
+const _load_data = Module.cwrap("load_data", "string", ["number", "number"]);
+
+const _load_meta = Module.cwrap("load_meta", "string", ["number", "number"]);
+const _meta_album_title = Module.cwrap("meta_album_title", "string", ["number"]);
+const _meta_author = Module.cwrap("meta_author", "string", ["number"]);
+const _meta_track_count = Module.cwrap("meta_track_count", "number", []);
+
+
+// Error handler, any non-null/non-empty string is an error
+const ERR_CHECK = (res: string) => {
+    if (res)
+        throw new Error(res);
+};
 
 
 /**
- *
+ * Load a VGM file remotely (or locally). Must have CORS permission.
  * @param url - url to load
  */
-export async function loadFile(url: string) {
+export async function loadVGM(url: string) {
     const {ptr, bytes} = await _cache.load(url);
 
-    const err = _loadData(ptr, bytes);
-    if (err)
-        throw Error(err);
+    ERR_CHECK(_load_data(ptr, bytes));
+}
+
+
+export namespace VgmMeta {
+    /**
+     * Load metadata. Must be successfully called before any other VgmMeta function
+     */
+    export async function load(file: File) {
+        const buf = await file.arrayBuffer();
+        const ptr = store(buf);
+
+        try {
+            ERR_CHECK(_load_meta(ptr, buf.byteLength));
+        } catch (e) {
+            let message = "vgm loading error";
+            if (e instanceof Error) {
+                message = e.message;
+            }
+
+            free(ptr);
+            return message;
+        }
+
+        free(ptr);
+        return "";
+    }
+
+    export function albumTitle() {
+        return _meta_album_title(0);
+    }
+
+    export function author(track: number = 0) {
+        return _meta_author(track);
+    }
+
+    export function trackCount() {
+        return _meta_track_count();
+    }
 }
