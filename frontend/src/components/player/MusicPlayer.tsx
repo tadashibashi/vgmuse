4a3a397bd4c01458edc689921740f6cf9823e726
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-28 02:36:17 -0700

    lcd screen is overflow hidden

--- frontend/src/components/player/MusicPlayer.tsx
+++ frontend/src/components/player/MusicPlayer.tsx
@@ -10,7 +10,7 @@ export default function() {
         <>
             <div className="flex flex-1 h-full">
 
-            <div className="grid grid-cols-6 grid-rows-1 h-full w-full">
+            <div className="grid grid-cols-8 grid-rows-1 h-full w-full gap-4">
 
                 {/*Controls*/}
                 <div className="col-span-2 flex p-0 m-0 h-full justify-center text-gray-500 gap-2">
@@ -23,7 +23,7 @@ export default function() {
 
 
                 {/*Track display (album art, name, composer, and time)*/}
-                <div className="col-span-3 flex items-center h-full p-0 m-0 group">
+                <div className="col-span-5 flex items-center h-full p-0 m-0 group">
                     <section className="flex flex-1 items-center w-full rounded-sm border-[1px] border-gray-100 h-[80%] p-0 m-0">
                         {/*  Album Art Square  */}
                         <section className="h-full aspect-square p-0 m-0">
@@ -36,9 +36,9 @@ export default function() {
                         <div className="h-full flex-grow p-0 m-0 w-auto">
 
                             {/* Title / Composer */}
-                            <div className="grid grid-rows-2 h-[91%] w-auto text-[13px]">
-                                <div className="flex items-center justify-center pt-1.5 text-gray-600">Octopath Traveler - Main Theme</div>
-                                <div className="flex items-center justify-center text-gray-400">Yasunori Nishiki –– Octopath Traveler - Main Theme</div>
+                            <div className="grid grid-rows-2 h-[91%] w-auto text-[13px] overflow-hidden">
+                                <div className="flex items-center justify-center pt-1.5 text-gray-600 whitespace-nowrap overflow-hidden">Octopath Traveler - Main Theme</div>
+                                <div className="flex items-center justify-center text-gray-400 whitespace-nowrap overflow-hidden">Yasunori Nishiki –– Octopath Traveler - Main Theme</div>
                             </div>
 
                             {/* Play bar */}
