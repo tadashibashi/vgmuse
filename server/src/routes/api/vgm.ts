58dd4de04d565178d20d69e98fd173b2acf1b07c
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-29 07:40:28 -0700

    add routes for vgm

--- server/src/routes/api/vgm.ts
+++ server/src/routes/api/vgm.ts
@@ -6,9 +6,12 @@ import {loginRequired} from "../../middleware/loginRequired";
 // Base URL: "/api/vgm"
 const router = express.Router();
 
+router.get("/:username", vgmCtrl.readAll);
 router.get("/:username/:slug", vgmCtrl.readOne);
 router.delete("/:username/:slug", loginRequired(), vgmCtrl.deleteOne);
 router.post("/", loginRequired(), formParser.files({limits: {fileSize: 2000000}}), vgmCtrl.createOne);
 router.patch("/:username/:slug", loginRequired(), formParser.files(), vgmCtrl.updateOne);
+
+router.get("/", vgmCtrl.search);
 
 export default router;
